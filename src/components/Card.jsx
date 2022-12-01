481bb8dc72517809c09bc68dcf66896fa87f0d8f
Danilo Leão <olin_ad@hotmail.com>
2022-11-30 23:10:59 -0300

    added preview layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,39 +15,85 @@ function Card(props) {
   } = props;
 
   return (
-    <StyledCard>
-      <div>
-        <h3 data-testid="name-card">{cardName}</h3>
-        <figure>
-          <img src={cardImage} alt={cardName} data-testid="description-card" />
-          <figcaption data-testid="rare-card">{cardRare}</figcaption>
-        </figure>
-      </div>
-      <div data-testid="description-card">
-        {cardDescription}
-      </div>
-      <div>
+    <StyledCardContainer>
+      <StyledCard>
         <div>
-          <p>Attr1</p>
-          <span data-testid="attr1-card">{cardAttr1}</span>
+          <h3 data-testid="name-card">{cardName}</h3>
+          <figure>
+            <img src={cardImage} alt={cardName} data-testid="description-card" />
+            <figcaption data-testid="rare-card">{cardRare}</figcaption>
+          </figure>
         </div>
-        <div>
-          <p>Attr2</p>
-          <span data-testid="attr2-card">{cardAttr2}</span>
+        <div data-testid="description-card">
+          <p>{cardDescription}</p>
         </div>
-        <div>
-          <p>Attr3</p>
-          <span data-testid="attr3-card">{cardAttr3}</span>
+        <div name="attributes">
+          <div>
+            <p>Attr1</p>
+            <span data-testid="attr1-card">{cardAttr1}</span>
+          </div>
+          <div>
+            <p>Attr2</p>
+            <span data-testid="attr2-card">{cardAttr2}</span>
+          </div>
+          <div>
+            <p>Attr3</p>
+            <span data-testid="attr3-card">{cardAttr3}</span>
+          </div>
+          <div>
+            <h3 data-testid="trunfo-card">{cardTrunfo ? 'Super Trunfo' : null}</h3>
+          </div>
         </div>
-        <div>
-          <h3 data-testid="trunfo-card">{cardTrunfo ? 'Super Trunfo' : null}</h3>
-        </div>
-      </div>
-    </StyledCard>
+      </StyledCard>
+    </StyledCardContainer>
   );
 }
 
+const StyledCardContainer = styled.div`
+  background-color: #333333;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Epilogue', sans-serif;
+`;
+
 const StyledCard = styled.div`
+  background-color: #035e96;
+  width: 50%;
+  height: 70%;
+  outline: 8px solid white;
+  border-radius: 20px;
+
+  & div[name=attributes] {
+    background-color: red;
+    /* clip-path: polygon(100% 0%, 100% 85%, 100% 50%, 0% 0%); */
+    margin-top: 15px;
+  }
+
+  & div:first-of-type h3 {
+    background-color: #012033;
+    color: white;
+    padding: 15px;
+    text-align: end;
+  }
+
+  & div:first-of-type figure {
+    width: 100%;
+    /* background-color: red; */
+  }
+
+  & div:first-of-type figure img {
+    width: 100%;
+    clip-path: polygon(100% 0%, 100% 85%, 0% 100%, 0% 0%);
+  }
+
+  & div:nth-of-type(2)[data-testid=description-card] {
+    background-color: green;
+    transform: rotateZ(-4.5deg) translateY(-20px);
+    padding: 10px;
+  }
 `;
 
 Card.propTypes = {
